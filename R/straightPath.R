60f9836b5e9888b4753502c78966cc0511dd9b70
TaikiSan21 <taiki.sakai@noaa.gov>
2019-07-26 12:49:16 -0700

    fix plotting for straightPath

--- R/straightPath.R
+++ R/straightPath.R
@@ -18,15 +18,27 @@
 #' @author Taiki Sakai \email{taiki.sakai@@noaa.gov}
 #'
 #' @importFrom RcppRoll roll_meanr
+#' @importFrom ggplot2 ggplot aes geom_path scale_color_manual
 #' @export
 #'
  straightPath<- function(gps, nSmall = 10, nLarge = 60, thresh = 10, plot = FALSE) {
     smallLag <- roll_meanr(gps$Heading, n = nSmall, fill = NA)
     bigLag <- roll_meanr(gps$Heading, n = nLarge, fill = NA)
-    gps$diff <- bigLag - smallLag
-    gps$straight <- gps$diff < thresh
+    gps$timeDiff <- gps$UTC - c(gps$UTC[1], gps$UTC[1:(nrow(gps)-1)])
+    gps$timeGroup <- as.factor(cumsum(gps$timeDiff > 10))
+    gps$headDiff <- bigLag - smallLag
+    gps$straight <- gps$headDiff < thresh
+    gps <- gps[-c(1:(nLarge-1)),]
     if(plot) {
-        plot(x=gps$Longitude, y=gps$Latitude, col=gps$straight, type='l')
-    }
-    gps[-c(1:(nLarge-1)),]
+        # strt <- gps[gps$straight, ]
+        # turn <- gps[!gps$straight, ]
+        # plot(x=strt$Longitude, y=strt$Latitude, col='darkgreen', type='l')
+        # lines(x=turn$Longitude, y=turn$Latitude, col='red', lwd = 2)
+        myPlot <- ggplot(gps, aes(x=Longitude, y=Latitude, col=straight)) +
+            geom_path(aes(group=timeGroup), size = 1.3) +
+            scale_color_manual(values = c('red', 'darkgreen'))
+            # scale_size_manual(values = c(2, 1))
+        print(myPlot)
+        }
+    gps
 }
