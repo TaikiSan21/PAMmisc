c64648ea2f9590811e36e79746829db4e8a81cbd
Taiki <tnsakai@gmail.com>
2026-02-04 16:38:29 -0800

    url updoots

--- R/getFigshareInfo.R
+++ R/getFigshareInfo.R
@@ -1,23 +1,23 @@
 #' @title getFigshareInfo
-#' 
+#'
 #' @description downloads filename and recording URL information from a Figshare
 #'   article. Requires a users API token from their figshare account
-#'   
+#'
 #' @param token Personal API token from users Figshare account, see
-#'   \href{https://help.figshare.com/article/how-to-get-a-personal-token}{here}
+#'   \href{https://info.figshare.com/user-guide/how-to-get-a-personal-token/}{here}
 #'   for information on creating a token
 #' @param id Figshare article ID to download information for
-#' 
+#'
 #' @author Taiki Sakai \email{taiki.sakai@@noaa.gov}
-#' 
+#'
 #' @return dataframe with columns \code{filename} and \code{recording_url}
-#' 
+#'
 #' @importFrom rjson fromJSON
 #' @importFrom httr GET
 #' @importFrom dplyr bind_rows
-#' 
+#'
 #' @export
-#' 
+#'
 getFigshareInfo <- function(token, id) {
     base <- "http://api.figshare.com/v2/"
     method <- paste("articles", id, sep = "/")
